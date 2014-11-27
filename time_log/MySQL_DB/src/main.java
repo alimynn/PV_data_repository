74ca444ed8d5ef01fc2cb0f10b0dcdff1371b90a
Stefan Walser <s.walser97@gmail.com>
2014-11-27 20:40:41 +0100

    Kommentare hinzugefügt!

--- time_log/MySQL_DB/src/main.java
+++ time_log/MySQL_DB/src/main.java
@@ -4,8 +4,7 @@ public class main {
 
 	public static void main(String[] args) throws IOException {
 		DBConnect connection = new DBConnect();
-		dataRead fstream = new dataRead();
-		
+		dataRead fstream = new dataRead();		
 		String[] content = new String[9];
 		
 		fstream.readFile(content);
@@ -14,7 +13,7 @@ public class main {
 		connection.writeData(Double.parseDouble(content[1])/1000, Integer.parseInt(content[3]), Double.parseDouble(content[4]));
 		
 		for(String x : content)
-			System.out.println(x);
+			System.out.println(x);		//Werte werden ausgegeben!
 	}
 	
 }
