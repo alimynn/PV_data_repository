1f8e9ec3f59b46541f1da96497e31c939fe3a331
Stefan Walser <s.walser97@gmail.com>
2014-11-27 18:39:18 +0100

    Initial check-in

---
+++ time_log/MySQL_DB/src/DBConnect.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class DBConnect {
+	
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	String db = "jdbc:mysql://192.168.1.35/solar";
+	String user ="solar";
+	String pw ="pvanlage";
+	
+	public DBConnect(){
+
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection(db, user, pw);
+			st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE , ResultSet.CONCUR_UPDATABLE);
+		} catch (Exception e) {
+			System.out.println("Error:" + e);
+		}
+	}
+	
+	public void writeData(double Wtoday, int Pac, double n) {
+		try {			
+			Date date = Calendar.getInstance().getTime();
+			Format formatter = new SimpleDateFormat("MMYY");
+			String datum = formatter.format(date);
+			
+			String query = "select * from statistik_"+datum+" where id=1";
+			rs = st. executeQuery(query);
+			/*System.out.println("Records from Database");
+			while (rs.next()) {
+				String leistung = rs.getString("leistung");
+				String spannung = rs.getString("spannung");
+				System.out.println("Leistung: " + leistung + "   Spannung: " + spannung +  "   Zeit: " + rs.getString("zeit"));
+			}*/
+			
+			rs.moveToInsertRow();
+			rs.updateDouble("Wtoday", Wtoday);
+			rs.updateInt("Pac", Pac);
+			rs.updateDouble("n", n);
+			rs.insertRow();
+			
+		
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
---
+++ time_log/MySQL_DB/src/dataRead.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class dataRead {
+	
+	public void readFile(String[] content) throws IOException {
+
+		Process proc = Runtime.getRuntime().exec("/home/solar/Piko/Piko.py --host 192.168.1.8 -t -i -d -p -q");
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				proc.getInputStream()));
+		String strLine = "";
+	
+		for(int i = 0; i < content.length; i++) {
+			if((strLine = in.readLine()) != null)
+				content[i] = strLine;
+			else
+				break;
+		}
+			
+	}
+	
+	public void filter(String[] content) {
+		
+		String[] temp = new String[content.length];
+		
+		for(int i = 0; i < temp.length; i++) {
+			temp[i] = content[i];
+			content[i] = "";
+		}
+		
+		content[0] = temp[0];
+		content[1] = temp[1];
+		
+		temp[2] += " ";
+		//char[] ctemp = temp[2].toCharArray();
+		String stemp = "";
+		int iter = 0;
+		for(int i = 0; i < temp[2].length(); i++) {
+			if(temp[2].charAt(i) == ' ') {
+				content[(2+iter)] = stemp;
+				iter++;
+				stemp = "";
+			}
+			else
+				stemp += temp[2].charAt(i);
+		}
+		
+		if(Double.parseDouble(content[4]) >= 100)
+		{
+			content[4] = "98.9";
+		}
+		
+		//ctemp = temp[3].toCharArray();
+		//char[] c2temp = temp[6].toCharArray();
+		//char[] c3temp = temp[7].toCharArray();
+		//char[] c4temp = temp[8].toCharArray();
+		
+		for(int i = 0; i < 5; i++) {
+			content[5] += temp[3].charAt(i);
+			content[6] += temp[6].charAt(i);
+			content[7] += temp[7].charAt(i);
+			content[8] += temp[8].charAt(i);
+		}
+	}
+
+}
