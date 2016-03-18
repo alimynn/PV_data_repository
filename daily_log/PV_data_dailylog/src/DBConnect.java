2483ea31f50e1dbc868dc89e8091637cafbbfa8b
Stefan Walser <s.walser97@gmail.com>
2016-03-18 20:52:47 +0100

    Anpassungen an den CentOS Server

--- daily_log/PV_data_dailylog/src/DBConnect.java
+++ daily_log/PV_data_dailylog/src/DBConnect.java
@@ -12,7 +12,7 @@ public class DBConnect {
 	private Connection con;
 	private Statement st;
 	private ResultSet rs;
-	String db = "jdbc:mysql://192.168.1.35/solar";
+	String db = "jdbc:mysql://localhost/solar";
 	String user = "solar";
 	String pw = "pvanlage";
 
@@ -29,11 +29,10 @@ public class DBConnect {
 		}
 	}
 
-	public void writeData(double Wtot, double Wtoday, java.sql.Date now,
-			int Pmax, int Pavg) {
+	public void writeData(double Wtot, double Wtoday, int Pmax, int Pavg) {
 		try {
 
-			String query = "select * from statistik_daily where id=1";
+/*			String query = "select * from statistik_daily where id=1";
 			rs = st.executeQuery(query);
 
 			rs.moveToInsertRow();
@@ -42,51 +41,54 @@ public class DBConnect {
 			rs.updateDouble("Wtoday", Wtoday);
 			rs.updateInt("Pmax", Pmax);
 			rs.updateInt("Pavg", Pavg);
-			rs.insertRow();
+			rs.insertRow();*/
+
+			String query = "INSERT into statistik_daily (Wtot, Wtoday, Pmax, Pavg) values (" + Wtot + "," + Wtoday + "," + Pmax + "," + Pavg + ")";
+			st.execute(query);
 
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 
-	public void readData(int[] p) {		
+	public void readData(int[] p) {
 		String[] temp = new String[60];
-		
+
 		try {
 			int i = 0;
-			
+
 			Date date2 = Calendar.getInstance().getTime();
-			Format formatter2 = new SimpleDateFormat("MMYY");
+			Format formatter2 = new SimpleDateFormat("YYMM");
 			String datum2 = formatter2.format(date2);
-			
-			Date date = Calendar.getInstance().getTime(); 
+
+			Date date = Calendar.getInstance().getTime();
 			Format formatter = new SimpleDateFormat("yyyy-MM-dd");
 			String datum = formatter.format(date); datum = datum.substring(5, 7) + datum.substring(8, 10);
 			String query = "select * from statistik_" + datum2;
 			String date21;
 			rs = st. executeQuery(query);
-			
+
 			while (rs.next()) {
 				String Pac = rs.getString("Pac");
 				String zeit = rs.getString("zeit");
 				date21 = zeit.substring(5, 7) + zeit.substring(8, 10);
-				
+
 				if(Integer.parseInt(datum) == Integer.parseInt(date21)) {
 					temp[i] = Pac;
 					i++;
-				}				
+				}
 
 			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 		for(int i = 0; i < temp.length; i++) {
 			if(temp[i] != null)
 				p[1] += Integer.parseInt(temp[i]);
 		}
 		p[1] = p[1] / 144;
-		
+
 		int itemp = 0;
 		for(int i = 0; i < temp.length; i++) {
 			if(temp[i] != null) {
@@ -94,9 +96,9 @@ public class DBConnect {
 					itemp = Integer.parseInt(temp[i]);
 			}
 		}
-		
-		p[0] = itemp;		
-		
+
+		p[0] = itemp;
+
 	}
 }
 
--- daily_log/PV_data_dailylog/src/DailyLog.java
+++ daily_log/PV_data_dailylog/src/DailyLog.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class main {
+public class DailyLog {
 
 	public static void main(String[] args) throws IOException {
 		DBConnect connection = new DBConnect();
@@ -8,16 +8,12 @@ public class main {
 		String[] content = new String[9];
 		int[] p = new int[2];
 
-		
-		java.util.Date now = new java.util.Date();
-		java.sql.Date date = new java.sql.Date(now.getTime());
-		
 		fstream.readFile(content);
 		fstream.filter(content);
 		connection.readData(p);
-		
-		connection.writeData(Double.parseDouble(content[0])/1000, Double.parseDouble(content[1])/1000, date, p[0], p[1]);
-		
+
+		connection.writeData(Double.parseDouble(content[0])/1000, Double.parseDouble(content[1])/1000, p[0], p[1]);
+
 	}
-	
+
 }
